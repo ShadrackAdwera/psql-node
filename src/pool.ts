263b102e11124c4c13a4c06812bfdf49d4d80483
ShadrackAdwera <adweshshaddie@gmail.com>
2022-12-23 16:12:30 +0300

    modify custom query wrapper function

--- src/pool.ts
+++ src/pool.ts
@@ -1,10 +1,24 @@
-import { Pool, PoolConfig } from 'pg';
+import { Pool, PoolConfig, QueryResult, QueryResultRow, QueryConfig } from 'pg';
+import { IUserData } from './types';
 
 class PgPool {
   _pool?: Pool;
 
   connect(options: PoolConfig) {
     this._pool = new Pool(options);
+    return this._pool.query(`SELECT 1 + 1`);
+  }
+
+  close() {
+    return this._pool?.end();
+  }
+
+  // TODO: REFECTOR THIS - SECURITY ISSUE
+  query<R extends QueryResultRow = any, I extends any[] = any[]>(
+    queryTextOrConfig: string | QueryConfig<I>,
+    values?: I
+  ): Promise<QueryResult<R>> {
+    return this._pool!.query(queryTextOrConfig);
   }
 }
 
