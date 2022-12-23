58cf75e549106843c44478833f1195062d5ccca3
ShadrackAdwera <adweshshaddie@gmail.com>
2022-12-23 16:12:49 +0300

    refactor fetch all users controller

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
+import { UserRepo } from '../repo/users';
+import { IUserData } from '../types';
+import { toCamelCase } from '../utils/toCamelCase';
 
-const getUsers = async (req: Request, res: Response, next: NextFunction) => {};
+const getUsers = async (req: Request, res: Response, next: NextFunction) => {
+  const users = await UserRepo.find();
+  const parsedUsers = toCamelCase<IUserData>(users);
+  res.status(200).json({ count: users.length, users: parsedUsers });
+};
 const getUser = async (req: Request, res: Response, next: NextFunction) => {};
 const addUser = async (req: Request, res: Response, next: NextFunction) => {};
 const updateUser = async (
