4421d2778fd56c6abd6c3ea748633ccb42a00f57
ShadrackAdwera <adweshshaddie@gmail.com>
2022-12-25 12:55:04 +0300

    write tests for delete / update users

--- src/controllers/tests/users.test.ts
+++ src/controllers/tests/users.test.ts
@@ -28,6 +28,39 @@ describe('user controllers', () => {
       expect(newTotalUsers).toEqual(1);
     });
   });
-  describe('update users controller', () => {});
-  describe('delete users controller', () => {});
+  describe('update users controller', () => {
+    it('should return a 404 when an invalid user ID is provided', async () => {
+      return request(app)
+        .patch(`${baseUrl}/0192837465`)
+        .send({ ...user, username: 'User Updated' })
+        .expect(404);
+    });
+    it('should return update a user', async () => {
+      const { body: postBody } = await request(app)
+        .post(baseUrl)
+        .send(user)
+        .expect(201);
+      expect(postBody.user.username).toEqual(user.username);
+      const { body: patchBody } = await request(app)
+        .patch(`${baseUrl}/${postBody.user.id}`)
+        .send({ ...user, username: 'User Updated' })
+        .expect(200);
+      expect(patchBody.user.username).toEqual('User Updated');
+    });
+  });
+  describe('delete users controller', () => {
+    it('should return a 404 when an invalid user ID is provided', async () => {
+      return request(app).delete(`${baseUrl}/0192837465`).send({}).expect(404);
+    });
+    it('should successfully delete a user', async () => {
+      const initialCount = await UserRepo.count();
+      expect(initialCount).toEqual(0);
+      const { body } = await request(app).post(baseUrl).send(user).expect(201);
+      const dbUsers = await UserRepo.count();
+      expect(dbUsers).toEqual(1);
+      await request(app).delete(`${baseUrl}/${body.user.id}`).expect(200);
+      const allUsers = await UserRepo.count();
+      expect(allUsers).toEqual(0);
+    });
+  });
 });
